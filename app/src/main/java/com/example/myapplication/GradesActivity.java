2a23199a7eeccb7810582fbee7c5ccfa0e459d76
Tomasz <tomekoleszko@gmail.com>
2022-03-17 18:57:05 +0100

    B&C

--- app/src/main/java/com/example/myapplication/GradesActivity.java
+++ app/src/main/java/com/example/myapplication/GradesActivity.java
@@ -1,19 +1,47 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class GradesActivity extends AppCompatActivity {
-
+    private ArrayList<ModelOceny> mSubjects;
+    private RecyclerView recyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_grades);
 
+        recyclerView = findViewById(R.id.lista_ocen_rv);
+        mSubjects = new ArrayList<>();
+
+        setSubjcetsInfo();
+        setAdapter();
+
+    }
+
+    private void setSubjcetsInfo() {
         String[] subjects = getResources().getStringArray(R.array.subjects);
         int subjectCount = Integer.parseInt(getIntent().getStringExtra(MainActivity.TEXT_KEY));
 
+        for (int i = 0; i < subjectCount; i++) {
+            mSubjects.add(new ModelOceny(subjects[i],2));
+        }
+    }
 
+    private void setAdapter() {
+        InteraktywnyAdapterTablicy adapterTablicy = new InteraktywnyAdapterTablicy(this, mSubjects);
+        recyclerView.setAdapter(adapterTablicy);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
--- app/src/main/java/com/example/myapplication/InteraktywnyAdapterTablicy.java
+++ app/src/main/java/com/example/myapplication/InteraktywnyAdapterTablicy.java
@@ -4,33 +4,36 @@ import android.app.Activity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import java.util.List;
+import java.util.ArrayList;
 
 public class InteraktywnyAdapterTablicy extends RecyclerView.Adapter<InteraktywnyAdapterTablicy.OcenyViewHolder> {
-    private List<ModelOceny> mListaOcen;
+
+    private ArrayList<ModelOceny> mListaOcen;
     private LayoutInflater mPompka;
 
-    public InteraktywnyAdapterTablicy(Activity kontekst, LayoutInflater mPompka) {
+    public InteraktywnyAdapterTablicy(Activity kontekst, ArrayList<ModelOceny> ListaOcen) {
         this.mPompka = kontekst.getLayoutInflater();
-        this.mListaOcen = mListaOcen;
+        this.mListaOcen = ListaOcen;
     }
 
     @NonNull
     @Override
     public InteraktywnyAdapterTablicy.OcenyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View wiersz = mPompka.inflate(R.layout.activity_grades,null);
+        View wiersz = mPompka.inflate(R.layout.model,parent,false);
         return new OcenyViewHolder(wiersz);
-
     }
 
     @Override
     public void onBindViewHolder(@NonNull OcenyViewHolder ocenyViewHolder, int numerWiersza){
-
+       String subject = mListaOcen.get(numerWiersza).getNazwa();
+       ocenyViewHolder.nameText.setText(subject);
     }
 
     @Override
@@ -39,14 +42,20 @@ public class InteraktywnyAdapterTablicy extends RecyclerView.Adapter<Interaktywn
     }
 
     public class OcenyViewHolder extends RecyclerView.ViewHolder implements  RadioGroup.OnCheckedChangeListener{
+        private final TextView nameText;
+        private RadioGroup radioGroup;
 
         @Override
-        public void onCheckedChanged(RadioGroup radioGroup, int i) {
-
+        public void onCheckedChanged(RadioGroup radioGroup, int checkedID) {
+            RadioButton radioButton = radioGroup.findViewById(checkedID);
         }
 
         public OcenyViewHolder(@NonNull View glownyElementWiersza){
             super(glownyElementWiersza);
+            nameText = itemView.findViewById(R.id.modeltextView);
+            radioGroup = itemView.findViewById(R.id.radioGroup1);
+
+            radioGroup.setOnCheckedChangeListener(this);
         }
     }
 }
