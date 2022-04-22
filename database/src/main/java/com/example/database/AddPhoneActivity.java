4c0436bcb5b557bbf724b3fd794ca3244271d016
Tomasz <tomekoleszko@gmail.com>
2022-04-22 21:03:19 +0200

    website button fixed

--- database/src/main/java/com/example/database/AddPhoneActivity.java
+++ database/src/main/java/com/example/database/AddPhoneActivity.java
@@ -1,6 +1,7 @@
 package com.example.database;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.webkit.URLUtil;
 import android.widget.Button;
@@ -12,7 +13,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class AddPhoneActivity extends AppCompatActivity {
 
-    private Button button_Cancel, button_Save, button_www;
     private EditText manufacturer_field, model_field, androidVersion_field, webSite_field;
     public static final String MANUFACTURER_KEY = "com.example.database.MANUFACTURER_KEY";
     public static final String MODEL_KEY = "com.example.database.MODEL_KEY";
@@ -29,9 +29,9 @@ public class AddPhoneActivity extends AppCompatActivity {
     }
 
     private void fields_setup() {
-        button_Cancel = findViewById(R.id.button_Cancel);
-        button_Save = findViewById(R.id.button_save);
-        button_www = findViewById(R.id.button_WebSite);
+        Button button_Cancel = findViewById(R.id.button_Cancel);
+        Button button_Save = findViewById(R.id.button_save);
+        Button button_www = findViewById(R.id.button_WebSite);
 
         manufacturer_field = findViewById(R.id.editTextManufacturer);
         model_field = findViewById(R.id.editTextModel);
@@ -64,5 +64,7 @@ public class AddPhoneActivity extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(), getString(R.string.emptyFields), Toast.LENGTH_LONG).show();
             }
         });
+
+        button_www.setOnClickListener(view -> startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(webSite_field.getText().toString()))));
     }
 }
