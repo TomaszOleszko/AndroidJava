ae50efb3e67d5d6c09f205e2c2c93360a8a9ab58
Tomasz <tomekoleszko@gmail.com>
2022-04-30 04:10:53 +0200

    Painter setup

---
+++ painter/src/main/java/com/example/painter/CustomSurfaceView.kt
@@ -0,0 +1,163 @@
+package com.example.painter
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.graphics.*
+import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.SurfaceHolder
+import android.view.SurfaceView
+import android.view.ViewConfiguration
+
+class CustomSurfaceView(context: Context, attrs: AttributeSet?) : SurfaceView(context, attrs),
+    SurfaceHolder.Callback {
+
+    private var path = Path()
+
+    private var mPojemnik: SurfaceHolder = holder
+    private lateinit var mWatekRysujacy: Thread
+    private var mWatekPracuje = false
+    private var mBlokada = Object()
+
+    private lateinit var mBitmapa: Bitmap
+    private lateinit var mKanwa: Canvas
+
+    private var drawColor = Color.BLACK
+    private val mPaintS = Paint().apply {
+        color = drawColor
+        style = Paint.Style.STROKE
+        strokeWidth = 2F
+    }
+    private val mPaintF = Paint().apply {
+        color = drawColor
+        style = Paint.Style.FILL
+        strokeWidth = 2F
+    }
+
+    fun changePaintColor(color: Int){
+        drawColor = color
+        mPaintS.color = drawColor
+        mPaintF.color = drawColor
+    }
+
+    private val touchTolerance = ViewConfiguration.get(context).scaledTouchSlop
+
+    private var currentX = 0f
+    private var currentY = 0f
+
+    private var motionTouchEventX = 0f
+    private var motionTouchEventY = 0f
+
+    private var screenHeight = 0
+    private var screenWidth = 0
+
+    init {
+        isFocusable = true
+        mPojemnik.addCallback(this)
+        setZOrderOnTop(true)
+        this.setBackgroundColor(Color.RED)
+    }
+    override fun onSizeChanged(width: Int, height: Int, oldWidth: Int, oldHeight: Int) {
+        super.onSizeChanged(width, height, oldWidth, oldHeight)
+
+        if (::mBitmapa.isInitialized) mBitmapa.recycle()
+        mBitmapa = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+        mKanwa = Canvas(mBitmapa)
+        mKanwa.drawColor(Color.BLUE)
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        canvas.drawBitmap(mBitmapa, 0f, 0f, null)
+    }
+
+    override fun surfaceCreated(holder: SurfaceHolder) {
+        mBitmapa = Bitmap.createBitmap(holder.surfaceFrame.width(),holder.surfaceFrame.height(), Bitmap.Config.ARGB_8888)
+        mKanwa = Canvas(mBitmapa)
+        mKanwa.drawColor(Color.BLUE)
+        resumeDrawing()
+        screenHeight = height
+        screenWidth = width
+    }
+
+    override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) {}
+
+    override fun surfaceDestroyed(holder: SurfaceHolder) {
+        pauseDrawing()
+    }
+
+    fun resumeDrawing() {
+       // mWatekRysujacy = Thread(this)
+        mWatekPracuje = true
+        mWatekRysujacy.start()
+    }
+
+    fun pauseDrawing() {
+        mWatekPracuje = false
+    }
+
+    fun clearCanva(){
+        mKanwa.drawColor(Color.WHITE)
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        performClick()
+
+        synchronized(mBlokada) {
+            if (event != null) {
+                motionTouchEventX = event.x
+                motionTouchEventY = event.y
+
+                when (event.action) {
+                    MotionEvent.ACTION_DOWN -> touchStart()
+                    MotionEvent.ACTION_MOVE -> touchMove()
+                    MotionEvent.ACTION_UP -> touchUp()
+                }
+            }
+
+        }
+        return true
+    }
+
+    private fun touchStart() {
+        path.reset()
+        path.moveTo(motionTouchEventX, motionTouchEventY)
+        currentX = motionTouchEventX
+        currentY = motionTouchEventY
+
+        mKanwa.drawCircle(currentX, currentY, 10F, mPaintF)
+    }
+
+    private fun touchMove() {
+        val dx = Math.abs(motionTouchEventX - currentX)
+        val dy = Math.abs(motionTouchEventY - currentY)
+        if (dx >= touchTolerance || dy >= touchTolerance) {
+            // QuadTo() adds a quadratic bezier from the last point,
+            // approaching control point (x1,y1), and ending at (x2,y2).
+            path.quadTo(
+                currentX,
+                currentY,
+                (motionTouchEventX + currentX) / 2,
+                (motionTouchEventY + currentY) / 2
+            )
+            currentX = motionTouchEventX
+            currentY = motionTouchEventY
+
+            mKanwa.drawPath(path, mPaintS)
+        }
+        // Invalidate() is inside the touchMove() under ACTION_MOVE because there are many other
+        // types of motion events passed into this listener, and we don't want to invalidate the
+        // view for those.
+        invalidate()
+    }
+
+    private fun touchUp() {
+        // Reset the path so it doesn't get drawn again.
+        mKanwa.drawCircle(currentX, currentY, 10F, mPaintF)
+        path.reset()
+    }
+
+
+
+
+}
--- painter/src/main/java/com/example/painter/MainActivity.kt
+++ painter/src/main/java/com/example/painter/MainActivity.kt
@@ -1,15 +1,64 @@
 package com.example.painter
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
 import android.os.Bundle
-import android.view.View.SYSTEM_UI_FLAG_FULLSCREEN
+import android.view.View
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.LinearLayout
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val canvaView = PowierzchniaRysunku(this)
-        canvaView.systemUiVisibility = SYSTEM_UI_FLAG_FULLSCREEN
-        canvaView.contentDescription = getString(R.string.desc)
-        setContentView(canvaView)
+        setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
+
+        val surfaceView = CustomSurfaceView(applicationContext, null)
+        val drawCanvas = findViewById<LinearLayout>(R.id.CustomSurfaceView)
+        drawCanvas.addView(surfaceView)
+
+        val button_RED = findViewById<Button>(R.id.button)
+        button_RED.setOnClickListener {
+            surfaceView.changePaintColor(Color.RED)
+        }
+
+        val button_YEL = findViewById<Button>(R.id.button2)
+        button_YEL.setOnClickListener {
+            surfaceView.changePaintColor(Color.YELLOW)
+        }
+
+        val button_BLU = findViewById<Button>(R.id.button3)
+        button_BLU.setOnClickListener {
+            surfaceView.changePaintColor(Color.BLUE)
+        }
+
+        val button_GRE = findViewById<Button>(R.id.button4)
+        button_GRE.setOnClickListener {
+            surfaceView.changePaintColor(Color.GREEN)
+        }
+
+        val button_CLEAR = findViewById<ImageButton>(R.id.imageButton)
+        button_CLEAR.setOnClickListener {
+            surfaceView.clearCanva()
+        }
+
+    }
+
+    private fun getBitmapFromView(view: View): Bitmap {
+        val returnedBitmap =
+            Bitmap.createBitmap(view.width, view.height, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(returnedBitmap)
+        val bgDrawable = view.background
+        if (bgDrawable != null) {
+            bgDrawable.draw(canvas)
+        } else {
+            canvas.drawColor(Color.WHITE)
+        }
+        view.draw(canvas)
+
+        return returnedBitmap
     }
 }
